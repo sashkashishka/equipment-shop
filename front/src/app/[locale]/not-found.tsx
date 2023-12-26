1d3d440d0f18880d353cd527fed01d2a5b417075
sashkashishka <alexandr1696@hotmail.com>
2023-12-27 00:59:27 +0100

    feat: not found styles

--- front/src/app/[locale]/not-found.tsx
+++ front/src/app/[locale]/not-found.tsx
@@ -1,4 +1,18 @@
-// TODO link and styles
+import { t } from 'ttag';
+import { Link } from '@/components/Link';
+import { ROUTES } from '@/constants/routes';
+
+import styles from './not-found.module.css';
+
 export default function NotFound() {
-  return <div>Page not found. go to main page</div>;
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>{t`Page not found`}</p>
+
+      <br />
+      <br />
+
+      <Link href={ROUTES.MAIN}>{t`Go to main page`}</Link>
+    </div>
+  );
 }
