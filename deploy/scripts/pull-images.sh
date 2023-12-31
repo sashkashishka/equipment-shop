906eb26818c2292b424f991086b68f37effbf990
sashkashishka <alexandr1696@hotmail.com>
2023-12-31 16:35:08 +0100

    ci: 64

--- deploy/scripts/pull-images.sh
+++ deploy/scripts/pull-images.sh
@@ -3,7 +3,15 @@
 front_version=$FRONT_VERSION
 strapi_version=$STRAPI_VERSION
 
+
+echo "FrontVersion:"
+echo $front_version
+
+echo "StrapiVersion:"
+echo $strapi_version
+
 docker pull sashkashishka/pgi-front:$front_version
 docker pull sashkashishka/pgi-strapi:$strapi_version
 
 # TODO delete outdated images
+echo "Pulled successfully"
